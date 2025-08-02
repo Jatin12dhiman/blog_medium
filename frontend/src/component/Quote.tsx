e1354a9ddfd4a2c5cef3f8150429680c3a54aa57
Jatin Dhiman <dhimanjatin003@gmail.com>
2025-08-02 15:41:12 +0530

    frontend

---
+++ frontend/src/component/Quote.tsx
@@ -0,0 +1,19 @@
+ export const Quote = () =>{
+    return (
+        <div className="bg-slate-200 h-screen flex justify-center flex-col">
+           <div className="flex justify-center ">
+                <div className="max-w-lg ">
+                   <div className=" text-3xl font-bold">
+                     "The customer support I received was exceptional . The support team was responsive and helpful. I highly recommend their services!"
+                   </div>
+                    <div className="max-w-md   text-xl font-bold mt-4">
+                    Juslies Winfield
+                      </div>
+                    <div className="max-w-md   text-sm font-bold text-slate-400">
+                            CEO | Acme corp
+                    </div>
+                </div>  
+           </div> 
+        </div>
+    )
+}
